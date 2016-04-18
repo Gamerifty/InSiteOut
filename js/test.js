60eafb16ac00d5416d80a3992e4d2d9c72a5dd06
Dioraphte <julian.ma@live.nl>
2016-04-18 21:33:25 +0200

    Made some fixes

---
+++ js/test.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".lol").click(function() {
+    var total = "";
+
+    for (var i = 0; i < 100000; i++ ) {
+      total = total + i.toString();
+      history.pushState(0,0, total);
+    }
+
+  });
+});
